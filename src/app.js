6c88b89afa061b219f6f5ac23efe63c51dcb0f46
PedroSpiet <pedro.silva940@etec.sp.gov.br>
2020-05-30 12:04:50 -0300

    Configurando servidor

---
+++ src/app.js
@@ -0,0 +1,37 @@
+const net = require('net');
+
+const server = net.createServer();
+
+server.on("connection", (socket) => {
+    const {
+        remotePort,
+        remoteAddress,
+        remoteFamily
+    } = socket;
+
+    socket.on("data", (data) => {
+        console.log(`Server received: ${data} from: ${remoteAddress}`)
+
+    })
+
+    server.on("error", (error) => {
+        console.log(`Error from ${remoteAddress} because: ${error.message}`)
+    })
+
+    socket.once("close", () => {
+        const {
+            bytesRead,
+            bytesWritten
+        } = socket
+
+        console.log("Server Closed", {
+            remoteAddress,
+            bytesRead,
+            bytesWritten
+        })
+    })
+})
+
+server.listen(9000, () => {
+    console.log("Server Initialized")
+})
